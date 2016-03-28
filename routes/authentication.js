860ca16233547195ba0ee759d83ac38da17b9bbf
Ganz7 <ganzse7en@gmail.com>
2016-03-28 15:38:18 -0500

    added db query

--- routes/authentication.js
+++ routes/authentication.js
@@ -11,6 +11,20 @@ router.get('/', function(request, response){
 	//var user_password = request.body.user_password;
 	var user_email = request.params.user_email;
 
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		client.query('SELECT * FROM user_table WHERE user_email=$1', [user_email] ,function(err, result) {
+      		done();
+      		if (err)
+       		{ 
+       			console.error(err); 
+       			response.send("Error " + err);  //Is this the proper way to report an error
+       		}
+      		else
+      		{ 
+      			response.send(result.rows);
+      		}
+    	});
+  	});
 	
 });
 
