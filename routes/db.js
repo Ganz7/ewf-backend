9c3091db72fd7836ea89fe9115a4fb7d92f7b311
Ganz7 <ganzse7en@gmail.com>
2016-03-28 14:52:44 -0500

    Indent fix

--- routes/db.js
+++ routes/db.js
@@ -3,18 +3,20 @@ var router = express.Router();
 var pg = require('pg');
 
 router.get('/', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM user_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { 
-       	//response.render('pages/db', {results: result.rows} ); 
-       	response.send(result.rows);
-       }
-    });
-  });
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		client.query('SELECT * FROM user_table', function(err, result) {
+      		done();
+      		if (err)
+       		{ 
+       			console.error(err); 
+       			response.send("Error " + err);  //Is this the proper way to report an error
+       		}
+      		else
+      		{ 
+      			response.send(result.rows);
+      		}
+    	});
+  	});
 })
 
 module.exports = router;
