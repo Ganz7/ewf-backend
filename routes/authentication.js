78391aecdb2fa80c07d99f00497a54a72011ee6a
Ganz7 <ganzse7en@gmail.com>
2016-03-28 15:23:58 -0500

    Added parameterized queries

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
+var bodyParser = require('body-parser');
 
-router.post('/', function(request, response){
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+router.get('/', function(request, response){
+	//var user_email = request.body.user_email;
+	//var user_password = request.body.user_password;
 	var user_email = request.params.user_email;
-	var user_password = request.params.user_password;
 
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		
+		client.query('SELECT * FROM user_table WHERE user_email=$1', [user_email] ,function(err, result) {
+      		done();
+      		if (err)
+       		{ 
+       			console.error(err); 
+       			response.send("Error " + err);  //Is this the proper way to report an error
+       		}
+      		else
+      		{ 
+      			response.send(result.rows);
+      		}
+    	});
   	});
 });
