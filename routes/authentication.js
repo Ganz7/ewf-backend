610d71e7f173be51f9433f14140e60757777bee3
Ganz7 <ganzse7en@gmail.com>
2016-04-09 21:31:30 -0500

    Working with query

--- routes/authentication.js
+++ routes/authentication.js
@@ -12,7 +12,7 @@ router.get('/', function(request, response){
 	var user_email = request.params.user_email;
 
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT * FROM user_table WHERE user_email=$1', [user_email] ,function(err, result) {
+		client.query('SELECT * FROM user_table WHERE user_email=$1::text', [user_email] ,function(err, result) {
       		done();
       		if (err)
        		{ 
