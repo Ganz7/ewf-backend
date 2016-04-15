bbbecbf22bac320bb32a995e5c3adc14cca128e3
Ganz7 <ganzse7en@gmail.com>
2016-04-15 15:58:40 -0500

    Added proper way to insert into event_table

--- routes/create.js
+++ routes/create.js
@@ -10,17 +10,18 @@ router.get('/event', function (request, response) {
 
 	var user_email = request.query.user_email;
 	var event_location = request.query.event_location;
-	var event_date = request.query.event_date;
 	var event_start_time = request.query.event_start_time;
 	var event_end_time = request.query.event_end_time;
 	var event_info = request.query.event_info;
 
-	if(isEmptyString(user_email) || isEmptyString(event_location) || isEmptyString(event_date)
-		|| isEmptyString(event_start_time) || isEmptyString(event_end_time)){
+	if(isEmptyString(user_email) || isEmptyString(event_location) || isEmptyString(event_start_time) 
+		|| isEmptyString(event_end_time)){
 		return response.send({'error': 'Incomplete Request'});
 	}
 
-	client.query('INSERT INTO user_table VALUES ($1, $2, $3)', [user_name, user_email, user_password], function(err, result){
+	client.query('INSERT INTO event_table(user_email, event_location, event_start_time, event_end_time
+		event_info) VALUES ($1, $2, $3, $4, $5) RETURNING _event_id', [user_email, event_location, event_start_time, 
+		event_end_time, event_info], function(err, result){
 		done();
 		if (err){ 
 			console.error(err); 
@@ -28,7 +29,8 @@ router.get('/event', function (request, response) {
 		}
 		else{
 			console.log("Successful Insertion");
-			return response.send({'success': 'New Account Created', 'user_name': user_name, 'user_email': user_email});
+			console.log(result);
+			return response.send({'success': 'Event Added', 'user_email': user_email, '_event_id': user_email});
 		}
     });
 
