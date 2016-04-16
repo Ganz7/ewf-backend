e0ac914c72b7827b5bbf9b64bac232e011876e49
Ganz7 <ganzse7en@gmail.com>
2016-04-16 00:12:00 -0500

    Missing closing paranthesis

--- routes/create.js
+++ routes/create.js
@@ -19,21 +19,26 @@ router.get('/event', function (request, response) {
 		return response.send({'error': 'Incomplete Request'});
 	}
 
-	client.query('INSERT INTO event_table(user_email, event_location, event_start_time, event_end_time, event_info) VALUES ($1, $2, $3, $4, $5) RETURNING _event_id', 
-		[user_email, event_location, event_start_time, 
-		event_end_time, event_info], function(err, result){
-		done();
-		if (err){ 
-			console.error(err); 
-			return response.send({'error': 'Internal Database Error'});
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		if(err){
+			console.error(err);
+			return response.send({'error': err});
 		}
-		else{
-			console.log("Successful Insertion");
-			console.log(result);
-			return response.send({'success': 'Event Added', 'user_email': user_email, '_event_id': user_email});
-		}
-    });
-
+		client.query('INSERT INTO event_table(user_email, event_location, event_start_time, event_end_time, event_info) VALUES ($1, $2, $3, $4, $5) RETURNING _event_id', 
+			[user_email, event_location, event_start_time, 
+			event_end_time, event_info], function(err, result){
+			done();
+			if (err){ 
+				console.error(err); 
+				return response.send({'error': 'Internal Database Error'});
+			}
+			else{
+				console.log("Successful Insertion");
+				console.log(result);
+				return response.send({'success': 'Event Added', 'user_email': user_email, '_event_id': user_email});
+			}
+	    });
+	});
 });
 
 
