62f49f36fae420248fb16aa915db01dbbdfd5bec
Ganz7 <ganzse7en@gmail.com>
2016-04-17 15:35:55 -0500

    Added new route for events

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var db = require('./routes/db');
 var authentication = require('./routes/authentication');
 var signup = require('./routes/signup');
 var create = require('./routes/create');
+var events = require('./routes/events');
 
 var app = express();
 
@@ -32,6 +33,7 @@ app.use('/db', db);
 app.use('/authentication', authentication);
 app.use('/signup', signup);
 app.use('/create', create);
+app.use('/events', events);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/events.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+
+router.get('/', function (request, response) {
+
+
+});
+
+module.exports = router;
